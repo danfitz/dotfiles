af03959d1a9b488657e89321c0236ebfc086c49b
Dan Fitz <dan@danfitz.com>
2019-10-06 12:00:26 -0400

    Added dropbox to startup and updated battest.sh script

--- battest.sh
+++ battest.sh
@@ -1,9 +1,21 @@
 BATPATH=/sys/class/power_supply/BAT0
-BAT_FULL=$BATPATH/charge_full
-BAT_NOW=$BATPATH/charge_now
-bf=$(cat $BAT_FULL)
-bn=$(cat $BAT_NOW)
-if [ $(( 100 * $bn / $bf  )) -lt 15  ]
-then
-    notify-send "Battery below 15%!"
-fi
+
+while true
+do
+    BAT_FULL=$BATPATH/charge_full
+    BAT_NOW=$BATPATH/charge_now
+    bf=$(cat $BAT_FULL)
+    bn=$(cat $BAT_NOW)
+
+    if [ $(( 100 * $bn / $bf  )) -lt 15 ]
+    then
+        notify-send "Battery Low! Plug in Charger!" "Power left: $((100 * $bn / $bf))%"
+    fi
+
+    if [ $(( 100 * $bn / $bf )) -gt 95 ]
+    then
+        notify-send "Battery Full! Unplug Charger!" "Power at: $((100 * $bn / $bf))%"
+    fi
+
+    sleep 5
+done
